1af94551296b1fa3f1782ddae33ed16d3a42b053
Fabio Dorakiewicz Alves <104926689+PaquiBik@users.noreply.github.com>
2023-05-30 18:30:25 -0300

    .

---
+++ .js
@@ -0,0 +1,10 @@
+var title = document.getElementById("neon-title");
+var colors = ["red", "blue", "purple"];
+var currentIndex = 0;
+
+function changeColor() {
+  title.style.color = colors[currentIndex];
+  currentIndex = (currentIndex + 1) % colors.length;
+}
+
+setInterval(changeColor, 3000);
